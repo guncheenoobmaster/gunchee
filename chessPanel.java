871bbf63d9700205e1e765a95a691f4f41d03dfe
guncenubmaster <147784772+guncheenoobmaster@users.noreply.github.com>
2023-10-13 11:14:14 +0800

    Add files via upload
    chessPanel

---
+++ chessPanel.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.goldthumb.chess;
+
+import javax.swing.JPanel;
+
+public class chessPanel extends JPanel{
+       
+}
