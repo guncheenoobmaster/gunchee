fe1208c2f5a0a48a5a8407a39c1d6b069294db8c
guncenubmaster <147784772+guncheenoobmaster@users.noreply.github.com>
2023-10-13 11:53:48 +0800

    Update chessPanel.java

--- chessPanel.java
+++ chessPanel.java
@@ -1,17 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.goldthumb.chess;
 
 import java.awt.Graphics;
-import java.awt.Graphics20;
+import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
-public class chessPanel extends JPanel{
-       @Override
-       proteted void paintChildren(Graphics g){
+public class chessPanel extends JPanel {
+    
+    @Override
+        protected void paintChildern(Graphics g) {
         super.paintChildren(g);
-        Graphics2D g2 = (Graphics2D);
-        g2.fillRect(0, 0, WIDTH, HEIGHT)
+        
+        Graphics2D g2 = (Graphics2D)g;
+        
+        g2.fillRect(0, 0, 600/8, 600/8);
        }
-
        
 }
